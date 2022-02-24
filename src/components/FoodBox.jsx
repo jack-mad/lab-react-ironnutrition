cb8096401f7cf5c1aadcabf6ff06ba11cb9a77d7
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-02-24 17:23:20 -0600

    Iteracion 1, 2

---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,32 @@
+
+import { Card, Col, Button } from 'antd';
+import foods from '../foods.json';
+function FoodBox(props) {
+  const items = props.foods
+  return (
+    <Col>
+    {  
+    items.map((item,index) => {
+      return(
+        <Card key={index}
+            title={item.name}
+            style={{ width: 230, height: 300, margin: 10 }}
+          >
+            <img src={item.image} height={60} alt="food" />
+            <p>Calories: {item.calories}</p>
+            <p>Servings: {item.servings}</p>
+            <p>
+              <b>Total Calories: {item.calories * item.servings} </b> kcal
+            </p>
+            <Button type="primary"> Delete </Button>
+          </Card>
+      )
+          
+        
+      })
+      }
+    </Col>
+    )
+}
+
+export default FoodBox;
